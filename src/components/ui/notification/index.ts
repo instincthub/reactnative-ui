045b0ac4665588c817afde7a423a539713940f5e
Noah Olatoye <noaholatoye@gmail.com>
2025-05-20 04:52:20 +0100

    Added useNotification

--- src/components/ui/notification/index.ts
+++ src/components/ui/notification/index.ts
@@ -1,4 +1,5 @@
 export { default as Notification } from "./Notification";
 export { default as NotificationProvider } from "./NotificationProvider";
+export { useNotification } from "./NotificationProvider";
 export { default as NotificationExampleScreen } from "./example/NotificationExampleScreen";
 export { default as NotificationExampleApp } from "./example/NotificationExampleApp";
